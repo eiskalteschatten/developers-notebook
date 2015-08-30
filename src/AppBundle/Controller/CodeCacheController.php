11867723ebadeddeb2bab177a82e8e4d874060f9
Alex <alex@alexseifert.com>
2015-08-30 11:19:10 +0200

    Added Code Cache

---
+++ src/AppBundle/Controller/CodeCacheController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class CodeCacheController extends Controller
+{
+    /**
+     * @Route("/code-cache/", name="codecache")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/code-cache.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+}
