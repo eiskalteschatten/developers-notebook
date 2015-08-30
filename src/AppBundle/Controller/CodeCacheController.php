7e279f41a30a312d3af78684df24573aa9375445
Alex <alex@alexseifert.com>
2015-08-30 20:25:18 +0200

    First stage of connecting the code cache to the database

--- src/AppBundle/Controller/CodeCacheController.php
+++ src/AppBundle/Controller/CodeCacheController.php
@@ -3,17 +3,40 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CodeCacheController extends Controller
 {
     /**
-     * @Route("/notebook/code-cache/", name="codecache")
+     * @Route("/notebook/code-cache/", name="codeCache")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+
         return $this->render('default/code-cache.html.twig');
+    }
+
+    /**
+     * @Route("/notebook/code-cache/create/", name="codeCacheCreate")
+     * @Method("POST")
+     */
+    public function createAction(Request $request)
+    {
+        $name = $request->request->get('name');
+
+        return new Response('success');
+    }
+
+    /**
+     * @Route("/notebook/code-cache/save/", name="codeCacheSave")
+     * @Method("POST")
+     */
+    public function saveAction(Request $request)
+    {
+
+        return new Response('success');
     }
 }
---
+++ src/AppBundle/Entity/CodeCacheEntry.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CodeCacheEntry
+ */
+class CodeCacheEntry
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $userId;
+
+    /**
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @var \DateTime
+     */
+    private $dateCreated;
+
+    /**
+     * @var \DateTime
+     */
+    private $dateModified;
+
+    /**
+     * @var integer
+     */
+    private $folder;
+
+    /**
+     * @var integer
+     */
+    private $project;
+
+    /**
+     * @var string
+     */
+    private $syntax;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return CodeCacheEntry
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return CodeCacheEntry
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set dateCreated
+     *
+     * @param \DateTime $dateCreated
+     * @return CodeCacheEntry
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreated
+     *
+     * @return \DateTime 
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * Set dateModified
+     *
+     * @param \DateTime $dateModified
+     * @return CodeCacheEntry
+     */
+    public function setDateModified($dateModified)
+    {
+        $this->dateModified = $dateModified;
+
+        return $this;
+    }
+
+    /**
+     * Get dateModified
+     *
+     * @return \DateTime 
+     */
+    public function getDateModified()
+    {
+        return $this->dateModified;
+    }
+
+    /**
+     * Set folder
+     *
+     * @param integer $folder
+     * @return CodeCacheEntry
+     */
+    public function setFolder($folder)
+    {
+        $this->folder = $folder;
+
+        return $this;
+    }
+
+    /**
+     * Get folder
+     *
+     * @return integer 
+     */
+    public function getFolder()
+    {
+        return $this->folder;
+    }
+
+    /**
+     * Set project
+     *
+     * @param integer $project
+     * @return CodeCacheEntry
+     */
+    public function setProject($project)
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    /**
+     * Get project
+     *
+     * @return integer 
+     */
+    public function getProject()
+    {
+        return $this->project;
+    }
+
+    /**
+     * Set syntax
+     *
+     * @param string $syntax
+     * @return CodeCacheEntry
+     */
+    public function setSyntax($syntax)
+    {
+        $this->syntax = $syntax;
+
+        return $this;
+    }
+
+    /**
+     * Get syntax
+     *
+     * @return string 
+     */
+    public function getSyntax()
+    {
+        return $this->syntax;
+    }
+}
