1fcd450015f6e83db7ed9a9a18450c66fe40a9dc
Alex <alex@alexseifert.com>
2015-08-31 13:21:27 +0200

    Pages are now shown in code cache

--- src/AppBundle/Controller/CodeCacheController.php
+++ src/AppBundle/Controller/CodeCacheController.php
@@ -14,13 +14,85 @@ use AppBundle\Entity\Projects;
 
 class CodeCacheController extends Controller
 {
+	private $standardSyntax = "javascript";
+	private $standardArea = "code";
+	private $dateTimeFormat = 'Y-m-d H:i:s';
+	
     /**
      * @Route("/notebook/code-cache/", name="codeCache")
      */
     public function indexAction(Request $request)
     {
+	    // GET PAGES
+	    
+		$pagesResult = $this->getDoctrine()
+        ->getRepository('AppBundle:Pages')
+        ->findBy(
+		    array('area' => $this->standardArea)//,
+		    //array('user_id' => 0)
+		);
+		
+		$pages = array();
+		
+		foreach ($pagesResult as $page) {
+			$content = $page->getContent();
+			$explodedContent = explode("\n", $content);
+			$previewContent = $explodedContent[0];	
+			
+			$pages[] = array(
+				'id' => $page->getId(),
+				'content' => $content,
+				'previewContent' => $previewContent,
+				'syntax' => $page->getSyntax(),
+				'folder' => $page->getFolder(),
+				'project' => $page->getProject(),
+				'date' => $page->getDateModified()->format($this->dateTimeFormat)
+			);
+		}
+		
+		 // GET FOLDERS
+		
+		$foldersResult = $this->getDoctrine()
+        ->getRepository('AppBundle:Folders')
+        ->findBy(
+		    array('area' => $this->standardArea)//,
+		    //array('user_id' => 0)
+		);
+		
+		$folders = array();
+		
+		foreach ($foldersResult as $folder) {
+			$folders[] = array(
+				'id' => $folder->getId(),
+				'name' => $folder->getName()
+			);
+		}
 
-        return $this->render('default/code-cache.html.twig');
+		// GET PROJECTS
+
+		$projectsResult = $this->getDoctrine()
+        ->getRepository('AppBundle:Projects')
+        ->findAll();
+/*
+        ->findBy(
+		    array('user_id' => 0)
+		);
+*/
+		
+		$projects = array();
+		
+		foreach ($projectsResult as $project) {
+			$projects[] = array(
+				'id' => $project->getId(),
+				'name' => $project->getName()
+			);
+		}
+		
+        return $this->render('default/code-cache.html.twig', array(
+	        'pages' => $pages,
+	        'folders' => $folders,
+	        'projects' => $projects
+        ));
     }
 
     /**
@@ -29,7 +101,6 @@ class CodeCacheController extends Controller
      */
     public function createAction(Request $request)
     {
-        //$name = $request->request->get('name');
         $date = new \DateTime("now");
 
         $pages = new Pages();
@@ -39,17 +110,17 @@ class CodeCacheController extends Controller
         $pages->setDateModified($date);
         $pages->setFolder(0);
         $pages->setProject(0);
-        $pages->setSyntax('javascript');
-        $pages->setArea('code');
+        $pages->setSyntax($this->standardSyntax);
+        $pages->setArea($this->standardArea);
 
         $em = $this->getDoctrine()->getManager();
 
         $em->persist($pages);
         $em->flush();
 
-        $response = new JsonResponse(array('id' => $pages->getId()));
+        $response = new JsonResponse(array('id' => $pages->getId(), 'syntax' => $pages->getSyntax(), 'folder' => $pages->getFolder(), 'project' => $pages->getProject(), 'date' => $pages->getDateModified()->format($this->dateTimeFormat)));
 
-        return new Response($response);
+        return $response;
     }
 
     /**
@@ -58,6 +129,7 @@ class CodeCacheController extends Controller
      */
     public function saveAction(Request $request)
     {
+		//$name = $request->request->get('name');
 
         return new Response('success');
     }
