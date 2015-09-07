650260b768cafba9a1b86877abfa5681030e6c38
Alex <alex@alexseifert.com>
2015-09-07 22:33:49 +0200

    Added Notebook sections

---
+++ src/AppBundle/Controller/NotebookController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Pages;
+use AppBundle\Entity\Folders;
+use AppBundle\Entity\Projects;
+use AppBundle\Entity\EditorSettings;
+use AppBundle\Services\Helper;
+
+class NotebookController extends Controller
+{
+    private $standardArea = "notebook";
+
+    /**
+     * @Route("/notebook/notebook/", name="notebook")
+     */
+    public function indexAction(Request $request)
+    {
+		$helper = $this->get('app.services.helper');
+
+        $dateTimeFormat = $this->container->getParameter('AppBundle.dateTimeFormat');
+
+		$user = $this->get('security.token_storage')->getToken()->getUser();
+		$userId = $user->getId();
+
+	    // GET PAGES
+	    
+		$pagesResult = $this->getDoctrine()
+        ->getRepository('AppBundle:Pages')
+        ->findBy(
+			array('userId' => $userId, 'area' => $this->standardArea),
+			array('dateModified' => 'DESC')
+		);
+		
+		$pages = array();
+		
+		foreach ($pagesResult as $page) {
+			$content = $page->getContent();
+			$previewContent = $helper->createPagePreview($content);
+			
+			$pages[] = array(
+				'id' => $page->getId(),
+				'content' => $content,
+				'previewContent' => $previewContent,
+				'syntax' => $page->getSyntax(),
+				'folder' => $page->getFolder(),
+				'project' => $page->getProject(),
+				'date' => $page->getDateModified()->format($dateTimeFormat)
+			);
+		}
+		
+		 // GET FOLDERS
+		
+		$foldersResult = $this->getDoctrine()
+        ->getRepository('AppBundle:Folders')
+        ->findBy(
+			array('userId' => $userId, 'area' => $this->standardArea),
+			array('name' => 'ASC')
+		);
+		
+		$folders = array();
+		
+		foreach ($foldersResult as $folder) {
+			$folders[] = array(
+				'id' => $folder->getId(),
+				'name' => $folder->getName()
+			);
+		}
+
+		// GET PROJECTS
+
+		$projectsResult = $this->getDoctrine()
+        ->getRepository('AppBundle:Projects')
+        ->findBy(
+			array('userId' => $userId),
+			array('name' => 'ASC')
+		);
+		
+		$projects = array();
+		
+		foreach ($projectsResult as $project) {
+			$projects[] = array(
+				'id' => $project->getId(),
+				'name' => $project->getName()
+			);
+		}
+		
+		// GET EDITOR SETTINGS
+
+		$settingsResult = $this->getDoctrine()
+        ->getRepository('AppBundle:EditorSettings')
+        ->findOneBy(
+			array('userId' => $userId)
+		);
+		
+        return $this->render('default/notebook.html.twig', array(
+            'standardArea' => $this->standardArea,
+	        'pages' => $pages,
+	        'folders' => $folders,
+	        'projects' => $projects,
+	        'editorSettings' => $settingsResult,
+			'syntaxOptions' => $this->container->getParameter('AppBundle.syntaxOptions'),
+			'editorThemes' => $this->container->getParameter('AppBundle.editorThemes')
+        ));
+    }
+}
