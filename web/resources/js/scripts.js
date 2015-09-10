32f24abd8faa0193e8599b0876624122aa357835
Alex <alex@alexseifert.com>
2015-09-10 11:46:06 +0200

    Journal, Code Cache, and Notes pages are now clickable and pre-selected in the single project view

--- web/resources/js/scripts.js
+++ web/resources/js/scripts.js
@@ -57,6 +57,21 @@ function closeAccountDropdown(button) {
 	});
 }
 
+function getUrlParameter(sParam) {
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+		sURLVariables = sPageURL.split('&'),
+		sParameterName,
+		i;
+
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=');
+
+		if (sParameterName[0] === sParam) {
+			return sParameterName[1] === undefined ? true : sParameterName[1];
+		}
+	}
+};
+
 // Functions for popups
 
 function openPopup(id) {
