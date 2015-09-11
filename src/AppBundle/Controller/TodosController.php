e05e9a0b8707c1d6b856d09b489ccab8ba072408
Alex <alex@alexseifert.com>
2015-09-11 16:30:01 +0200

    TodosController added, not yet really functional

---
+++ src/AppBundle/Controller/TodosController.php
@@ -0,0 +1,315 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Todo;
+use AppBundle\Entity\Folders;
+use AppBundle\Entity\Project;
+use AppBundle\Services\Helper;
+
+class TodosController extends Controller
+{
+    private $standardArea = "todos";
+	private $currentPage = "todos";
+
+    /**
+     * @Route("/notebook/todos/", name="todos")
+     */
+    public function indexAction(Request $request)
+    {
+		$helper = $this->get('app.services.helper');
+
+        $dateTimeFormat = $this->container->getParameter('AppBundle.dateTimeFormat');
+
+		$user = $this->get('security.token_storage')->getToken()->getUser();
+		$userId = $user->getId();
+
+	    // GET TO DOS
+	    
+		$todosResult = $this->getDoctrine()
+        ->getRepository('AppBundle:Todo')
+        ->findBy(
+			array('userId' => $userId),
+			array('dateModified' => 'DESC')
+		);
+
+		$todos = array();
+		
+		foreach ($todosResult as $todo) {
+			$todos[] = array(
+				'id' => $todo->getId(),
+				'name' => $todo->getTodo(),
+				'notes' => $todo->getNotes(),
+				'isCompleted' => $todo->getIsCompleted(),
+				'dateCompleted' => $todo->getDateCompleted()->format($dateTimeFormat),
+				'datePlanned' => $todo->getDatePlanned()->format($dateTimeFormat),
+				'priority' => $todo->getPriority(),
+				'folder' => $todo->getFolder(),
+				'project' => $todo->getProject(),
+				'date' => $todo->getDateModified()->format($dateTimeFormat)
+			);
+		}
+		
+		// GET FOLDERS
+		
+		$foldersResult = $this->getDoctrine()
+        ->getRepository('AppBundle:Folders')
+        ->findBy(
+			array('userId' => $userId, 'area' => $this->standardArea),
+			array('name' => 'ASC')
+		);
+		
+		$folders = array();
+		
+		foreach ($foldersResult as $folder) {
+			$folders[] = array(
+				'id' => $folder->getId(),
+				'name' => $folder->getName()
+			);
+		}
+
+		// GET PROJECTS
+
+		$projectsResult = $this->getDoctrine()
+        ->getRepository('AppBundle:Project')
+        ->findBy(
+			array('userId' => $userId),
+			array('name' => 'ASC')
+		);
+		
+		$projects = array();
+		
+		foreach ($projectsResult as $project) {
+			if (!$project->getIsCompleted()) {
+				$projects[] = array(
+					'id' => $project->getId(),
+					'name' => $project->getName()
+				);
+			}
+		}
+
+        return $this->render('default/todos.html.twig', array(
+			'todos' => $todos,
+			'folders' => $folders,
+			'projects' => $projects,
+            'standardArea' => $this->standardArea,
+			'currentPage' => $this->currentPage
+        ));
+    }
+
+	/**
+	 * @Route("/notebook/todos/movePageToFolder/", name="todosMovePageToFolder")
+	 * @Method("POST")
+	 */
+	public function movePageToFolderAction(Request $request)
+	{
+		$folderId = $request->request->get('folderId');
+		$pageId = $request->request->get('pageId');
+
+		$em = $this->getDoctrine()->getManager();
+		$pages = $em->getRepository('AppBundle:Todo')->find($pageId);
+
+		$pages->setFolder($folderId);
+		$em->flush();
+
+		$response = new JsonResponse(array('folder' => $pages->getFolder()));
+
+		return $response;
+	}
+
+	/**
+	 * @Route("/notebook/todos/removePageFromFolders/", name="todosRemovePageFromFolders")
+	 * @Method("POST")
+	 */
+	public function removePageFromFoldersAction(Request $request)
+	{
+		$folderId = -1;
+		$pageId = $request->request->get('pageId');
+
+		$em = $this->getDoctrine()->getManager();
+		$pages = $em->getRepository('AppBundle:Todo')->find($pageId);
+
+		$pages->setFolder($folderId);
+		$em->flush();
+
+		$response = new JsonResponse(array('folder' => $pages->getFolder()));
+
+		return $response;
+	}
+
+	/**
+	 * @Route("/notebook/todos/createFolder/", name="todosCreateFolder")
+	 * @Method("POST")
+	 */
+	public function createFolderAction(Request $request)
+	{
+		$standardArea = $request->request->get('standardArea');
+		$name = $request->request->get('name');
+
+		$date = new \DateTime("now");
+
+		$user = $this->get('security.token_storage')->getToken()->getUser();
+		$userId = $user->getId();
+
+		$folders = new Folders();
+		$folders->setUserId($userId);
+		$folders->setDateCreated($date);
+		$folders->setDateModified($date);
+		$folders->setName($name);
+		$folders->setArea($standardArea);
+
+		$em = $this->getDoctrine()->getManager();
+
+		$em->persist($folders);
+		$em->flush();
+
+		$response = new JsonResponse(array('id' => $folders->getId(), 'name' => $folders->getName()));
+
+		return $response;
+	}
+
+	/**
+	 * @Route("/notebook/todos/removeFolder/", name="todosRemoveFolder")
+	 * @Method("POST")
+	 */
+	public function removeFolderAction(Request $request)
+	{
+		$id = $request->request->get('id');
+
+		$em = $this->getDoctrine()->getManager();
+		$folders = $em->getRepository('AppBundle:Folders')->find($id);
+
+		$em->remove($folders);
+
+		$pagesResult = $this->getDoctrine()
+			->getRepository('AppBundle:Todo')
+			->findBy(
+				array('folder' => $id)
+			);
+
+		foreach($pagesResult as $page) {
+			$page->setFolder(-1);
+		}
+
+		$em->flush();
+
+		return new Response('success');
+	}
+
+	/**
+	 * @Route("/notebook/todos/createTodo/", name="todosCreateTodo")
+	 * @Method("POST")
+	 */
+	public function createTodoAction(Request $request)
+	{
+		$project = $request->request->get('project');
+		$folder = $request->request->get('folder');
+
+		$date = new \DateTime("now");
+
+		$user = $this->get('security.token_storage')->getToken()->getUser();
+		$userId = $user->getId();
+
+		$todo = new Todo();
+		$todo->setUserId($userId);
+		$todo->setDateCreated($date);
+		$todo->setDateModified($date);
+		$todo->setTodo("");
+		$todo->setNotes("");
+		$todo->setIsCompleted(false);
+		$todo->setPriority(1);
+		$todo->setProject($project);
+		$todo->setFolder($folder);
+
+		$em = $this->getDoctrine()->getManager();
+
+		$em->persist($todo);
+		$em->flush();
+
+		$response = new JsonResponse(array('id' => $todo->getId(), 'project' => $todo->getProject(), 'folder' => $todo->getFolder(), 'priority' => $todo->getPriority(), 'isCompleted' => $todo->getIsCompleted()));
+
+		return $response;
+	}
+
+	/**
+	 * @Route("/notebook/todos/saveTodo/", name="todosSaveTodo")
+	 * @Method("POST")
+	 */
+	public function saveTodoAction(Request $request)
+	{
+		$id = $request->request->get('id');
+		$name = $request->request->get('name');
+		$url = $request->request->get('url');
+		$notes = $request->request->get('notes');
+
+		$date = new \DateTime("now");
+
+		$user = $this->get('security.token_storage')->getToken()->getUser();
+		$userId = $user->getId();
+
+		$em = $this->getDoctrine()->getManager();
+		$bookmark = $em->getRepository('AppBundle:Todo')->find($id);
+
+		if (!$bookmark) {
+			throw $this->createNotFoundException('No to do found for id '.$id);
+		}
+
+		$bookmark->setDateModified($date);
+		$bookmark->setName($name);
+		$bookmark->setUrl($url);
+		$bookmark->setNotes($notes);
+
+		$em->flush();
+
+		$response = new JsonResponse(array('id' => $bookmark->getId(), 'name' => $bookmark->getName(), 'url' => $bookmark->getUrl(), 'croppedUrl' => $helper->cropBookmarkUrl($bookmark->getUrl()), 'notes' => $bookmark->getNotes()));
+
+		return $response;
+	}
+
+	/**
+	 * @Route("/notebook/todos/remove/", name="todosRemove")
+	 * @Method("POST")
+	 */
+	public function removeAction(Request $request)
+	{
+		$id = $request->request->get('id');
+
+		$em = $this->getDoctrine()->getManager();
+		$bookmark = $em->getRepository('AppBundle:Todo')->find($id);
+
+		if (!$bookmark) {
+			throw $this->createNotFoundException('No to do found for id '.$id);
+		}
+
+		$em->remove($bookmark);
+		$em->flush();
+
+		return new Response('success');
+	}
+
+	/**
+	 * @Route("/notebook/todos/movePageToProject/", name="todosMovePageToProject")
+	 * @Method("POST")
+	 */
+	public function movePageToProjectAction(Request $request)
+	{
+		$projectId = $request->request->get('projectId');
+		$pageId = $request->request->get('pageId');
+
+		$em = $this->getDoctrine()->getManager();
+		$pages = $em->getRepository('AppBundle:Todo')->find($pageId);
+
+		$pages->setProject($projectId);
+		$em->flush();
+
+		$response = new JsonResponse(array('project' => $pages->getProject()));
+
+		return $response;
+	}
+}
---
+++ src/AppBundle/Entity/Todo.php
@@ -0,0 +1,335 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Todo
+ */
+class Todo
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $userId;
+
+    /**
+     * @var \DateTime
+     */
+    private $dateCreated;
+
+    /**
+     * @var \DateTime
+     */
+    private $dateModified;
+
+    /**
+     * @var string
+     */
+    private $todo;
+
+    /**
+     * @var string
+     */
+    private $notes;
+
+    /**
+     * @var boolean
+     */
+    private $isCompleted;
+
+    /**
+     * @var \DateTime
+     */
+    private $dateCompleted;
+
+    /**
+     * @var \DateTime
+     */
+    private $datePlanned;
+
+    /**
+     * @var integer
+     */
+    private $priority;
+
+    /**
+     * @var integer
+     */
+    private $folder;
+
+    /**
+     * @var integer
+     */
+    private $project;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Todo
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set dateCreated
+     *
+     * @param \DateTime $dateCreated
+     * @return Todo
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreated
+     *
+     * @return \DateTime 
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * Set dateModified
+     *
+     * @param \DateTime $dateModified
+     * @return Todo
+     */
+    public function setDateModified($dateModified)
+    {
+        $this->dateModified = $dateModified;
+
+        return $this;
+    }
+
+    /**
+     * Get dateModified
+     *
+     * @return \DateTime 
+     */
+    public function getDateModified()
+    {
+        return $this->dateModified;
+    }
+
+    /**
+     * Set todo
+     *
+     * @param string $todo
+     * @return Todo
+     */
+    public function setTodo($todo)
+    {
+        $this->todo = $todo;
+
+        return $this;
+    }
+
+    /**
+     * Get todo
+     *
+     * @return string 
+     */
+    public function getTodo()
+    {
+        return $this->todo;
+    }
+
+    /**
+     * Set notes
+     *
+     * @param string $notes
+     * @return Todo
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    /**
+     * Get notes
+     *
+     * @return string 
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+
+    /**
+     * Set isCompleted
+     *
+     * @param boolean $isCompleted
+     * @return Todo
+     */
+    public function setIsCompleted($isCompleted)
+    {
+        $this->isCompleted = $isCompleted;
+
+        return $this;
+    }
+
+    /**
+     * Get isCompleted
+     *
+     * @return boolean 
+     */
+    public function getIsCompleted()
+    {
+        return $this->isCompleted;
+    }
+
+    /**
+     * Set dateCompleted
+     *
+     * @param \DateTime $dateCompleted
+     * @return Todo
+     */
+    public function setDateCompleted($dateCompleted)
+    {
+        $this->dateCompleted = $dateCompleted;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCompleted
+     *
+     * @return \DateTime 
+     */
+    public function getDateCompleted()
+    {
+        return $this->dateCompleted;
+    }
+
+    /**
+     * Set datePlanned
+     *
+     * @param \DateTime $datePlanned
+     * @return Todo
+     */
+    public function setDatePlanned($datePlanned)
+    {
+        $this->datePlanned = $datePlanned;
+
+        return $this;
+    }
+
+    /**
+     * Get datePlanned
+     *
+     * @return \DateTime 
+     */
+    public function getDatePlanned()
+    {
+        return $this->datePlanned;
+    }
+
+    /**
+     * Set priority
+     *
+     * @param integer $priority
+     * @return Todo
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * Get priority
+     *
+     * @return integer 
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * Set folder
+     *
+     * @param integer $folder
+     * @return Todo
+     */
+    public function setFolder($folder)
+    {
+        $this->folder = $folder;
+
+        return $this;
+    }
+
+    /**
+     * Get folder
+     *
+     * @return integer 
+     */
+    public function getFolder()
+    {
+        return $this->folder;
+    }
+
+    /**
+     * Set project
+     *
+     * @param integer $project
+     * @return Todo
+     */
+    public function setProject($project)
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    /**
+     * Get project
+     *
+     * @return integer 
+     */
+    public function getProject()
+    {
+        return $this->project;
+    }
+}
