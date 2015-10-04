32b3c43ed5cfa74808bd1a9fdb25734ec7468ab9
Alex <alex@alexseifert.com>
2015-10-04 19:09:08 +0200

    Searching to dos, issues and bookmarks now works (#14) - only projects are still missing

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -57,7 +57,7 @@ class SearchController extends Controller
 				'id' => $result['id'],
 				'previewContent' => $previewContent,
 				'area' => $result['area'],
-				'date' => $result['dateModified']->format($dateTimeFormat),
+				'date' => $result['dateModified']->format($dateTimeFormat)
 			);
 		}
 
@@ -65,5 +65,111 @@ class SearchController extends Controller
 
 		return $response;
 	}
+    
+	/**
+	 * @Route("/notebook/search/todosIssuesBookmarks/", name="searchTodosIssuesBookmarks")
+	 * @Method("POST")
+	 */
+	public function searchTodosIssuesBookmarksAction(Request $request)
+	{
+		$q = $request->request->get('q');
+		$searchTerm = "%" . $q . "%";
+		
+		$helper = $this->get('app.services.helper');
+		
+        $dateFormat = $this->container->getParameter('AppBundle.dateFormat');
+        $numberOfItems = $this->container->getParameter('AppBundle.searchNumberOfResults');
 
+		$user = $this->get('security.token_storage')->getToken()->getUser();
+		$userId = $user->getId();
+
+		$em = $this->getDoctrine()->getManager();
+		
+		$results = array();
+		
+		// GET TO DOS
+		
+		$query = $em->createQuery("SELECT t.id, t.userSpecificId, t.todo, t.priority, t.datePlanned, t.dateDue FROM AppBundle:Todo t WHERE (t.todo LIKE :searchTerm OR t.notes LIKE :searchTerm) AND t.userId = :userId AND t.isCompleted = false")
+			->setParameter('searchTerm', $searchTerm)
+			->setParameter('userId', $userId)
+			->setMaxResults($numberOfItems);
+		$searchResult = $query->getResult();
+
+		foreach ($searchResult as $result) {
+			$datePlanned = $result['datePlanned'];
+			if ($datePlanned) {
+				$datePlanned = $datePlanned->format($dateFormat);
+			}
+
+			$dateDue = $result['dateDue'];
+			if ($dateDue) {
+				$dateDue = $dateDue->format($dateFormat);
+			}
+			
+			$results[] = array(
+				'id' => $result['id'],
+				'itemId' => $result['userSpecificId'],
+				'name' => $result['todo'],
+				'priority' => $result['priority'],
+				'area' => 'todo',
+				'datePlanned' => $datePlanned,
+				'dateDue' => $dateDue	
+			);
+		}
+		
+		
+		// GET ISSUES
+		
+		$query = $em->createQuery("SELECT t.id, t.userSpecificId, t.title, t.description, t.datePlanned, t.dateDue FROM AppBundle:Issue t WHERE (t.title LIKE :searchTerm OR t.description LIKE :searchTerm) AND t.userId = :userId AND t.isCompleted = false")
+			->setParameter('searchTerm', $searchTerm)
+			->setParameter('userId', $userId)
+			->setMaxResults($numberOfItems);
+		$searchResult = $query->getResult();
+
+		foreach ($searchResult as $result) {
+			$datePlanned = $result['datePlanned'];
+			if ($datePlanned) {
+				$datePlanned = $datePlanned->format($dateFormat);
+			}
+
+			$dateDue = $result['dateDue'];
+			if ($dateDue) {
+				$dateDue = $dateDue->format($dateFormat);
+			}
+			
+			$results[] = array(
+				'id' => $result['id'],
+				'itemId' => $result['userSpecificId'],
+				'name' => $result['title'],
+				'description' => $result['description'],
+				'area' => 'issues',
+				'datePlanned' => $datePlanned,
+				'dateDue' => $dateDue		
+			);
+		}
+		
+		
+		// GET BOOKMARKS
+		
+		$query = $em->createQuery("SELECT t.id, t.name, t.url FROM AppBundle:Bookmark t WHERE (t.name LIKE :searchTerm OR t.url LIKE :searchTerm OR t.notes LIKE :searchTerm) AND t.userId = :userId")
+			->setParameter('searchTerm', $searchTerm)
+			->setParameter('userId', $userId)
+			->setMaxResults($numberOfItems);
+		$searchResult = $query->getResult();
+
+		foreach ($searchResult as $result) {
+			$results[] = array(
+				'id' => $result['id'],
+				'name' => $result['name'],
+				'url' => $result['url'],
+                'croppedUrl' => $helper->cropBookmarkUrl($result["url"]),
+				'area' => 'bookmarks'
+			);
+		}
+
+
+		$response = new JsonResponse($results);
+
+		return $response;
+	}
 }
